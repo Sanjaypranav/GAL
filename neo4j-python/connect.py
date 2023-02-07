03e8bcbf67e08583e9872c836429fd4bcb674e9f
Sanjaypranav <sanjaypriya195@gmail.com>
2023-02-07 09:39:01 +0530

    neo4j updated

---
+++ neo4j-python/connect.py
@@ -0,0 +1,39 @@
+from neo4j import GraphDatabase
+
+class HelloWorldExample:
+
+    def __init__(self, uri, user, password):
+        self.driver = GraphDatabase.driver(uri, auth=(user, password))
+
+    def close(self):
+        self.driver.close()
+
+    def add_student(self, message):
+        with self.driver.session() as session:
+            greeting = session.execute_write(self.add_student_to_db, message)
+            print(greeting)
+
+    @staticmethod
+    def add_student_to_db(tx, message):
+        result = tx.run("""// create a database 
+        CREATE (a:Student {name: $message})
+        WITH a
+        match (n) return n;
+        """, message=message)
+        return result.single()[0]
+
+# Create a student database table using neo4j 
+
+
+
+if __name__ == "__main__":
+    greeter = HelloWorldExample("bolt://localhost:7687", "neo4j", "2509")
+    # Print Database name
+    while True:
+        print("Enter the name of the student")
+        name = input()
+        greeter.add_student(name)
+        print("Do you want to add another student? (y/n)")
+        if input() == "n":
+            break
+    greeter.close()
